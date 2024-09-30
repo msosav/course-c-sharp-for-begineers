e88b5663a3479f876aca3fb0f1d23d366f39a6f3
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-09-30 12:47:08 -0500

    Add Lesson 14: Lists of Other Types

---
+++ 14Fibonacci/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Fibonacci {
+  class Program {
+    static void Main(string[] args) {
+      var fibonacciNumbers = new List<int> {0, 1};
+      for (int i = 0; i < 18; i++) {
+        var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+        var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+        fibonacciNumbers.Add(previous + previous2);
+        Console.WriteLine(fibonacciNumbers[i]);
+      }
+    }
+  }
+}
