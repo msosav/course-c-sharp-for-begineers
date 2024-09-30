2ef82faca8850df786885fb57fc07863c754f0e2
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-09-30 12:27:57 -0500

    Add Lesson 12: Arrays, List, and Collections

---
+++ 12DataStructures/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructures
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var names = new List<string> { "<name>", "Ana", "Felipe" };
+      foreach (var name in names)
+      {
+        Console.WriteLine($"Hello {name.ToUpper()}!");
+      }
+
+      Console.WriteLine();
+
+      for (int i = 0; i < names.Count; i++)
+      {
+        Console.WriteLine($"Hello {names[i].ToUpper()}!");
+      }
+
+      Console.WriteLine();
+
+      names.Add("Maria");
+      names.Add("Bill");
+      names.Remove("Ana");
+      foreach (var name in names)
+      {
+        Console.WriteLine($"Hello {name.ToUpper()}!");
+      }
+    }
+  }
+}
