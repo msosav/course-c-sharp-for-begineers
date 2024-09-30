6b15469706b3ffe4dbca9b1ad8cce2e208fb28fb
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-09-30 11:44:19 -0500

    Add Lesson 07: Numbers and Decimals

---
+++ 07Decimals/Program.cs
@@ -0,0 +1,20 @@
+﻿// Decimals have a higher precision than floats and doubles, and are typically used in financial and monetary calculations.
+decimal min = decimal.MinValue;
+decimal max = decimal.MaxValue;
+
+Console.WriteLine($"The range of the decimal type is {min} to {max}\n");
+
+double a = 1.0;
+double b = 3.0;
+Console.WriteLine(a / b);
+Console.WriteLine();
+
+decimal c = 1.0M;
+decimal d = 3.0M;
+Console.WriteLine(c / d);
+Console.WriteLine();
+
+// Challenge: Write a program that calculates the area of a circle.
+double radius = 2.50;
+double area = Math.PI * radius * radius;
+Console.WriteLine($"The area of a circle with a radius of {radius} is {area}");
