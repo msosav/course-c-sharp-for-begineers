0fa4d622d457c96632bd4f7ff915edc3b6d9dd4a
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-09-30 10:34:02 -0500

    Add Lesson 04: Searching Strings

---
+++ 04SearchingStrings/Program.cs
@@ -0,0 +1,9 @@
+﻿string songLyrics = "You say goodbye, and I say hello";
+
+Console.WriteLine(songLyrics);
+Console.WriteLine($"Contains goodbye: {songLyrics.Contains("goodbye")}");
+Console.WriteLine($"Contains greetings: {songLyrics.Contains("greeetings")}");
+Console.WriteLine($"Starts with You: {songLyrics.StartsWith("You")}");
+Console.WriteLine($"Ends with hello: {songLyrics.EndsWith("hello")}");
+Console.WriteLine($"Ends with goodbye: {songLyrics.EndsWith("goodbye")}");
+
