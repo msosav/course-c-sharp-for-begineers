be91278a9b66599af3a9d5dbae1b615d90cab76f
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-09-30 09:13:25 -0500

    Add Lesson 03: The Basics of Strings

---
+++ 03Strings/Program.cs
@@ -0,0 +1,36 @@
+﻿string firstFriend = "Bill";
+string secondFriend = "Maira";
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}\n");
+
+// Length of a string
+
+Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.\n");
+
+// Trailing spaces
+
+string greeting = "     Hello World!     ";
+Console.WriteLine($"[{greeting}]");
+
+string trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.TrimEnd();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.Trim();
+Console.WriteLine($"[{trimmedGreeting}]\n");
+
+// Replace
+
+string sayHello = "Hello World!";
+Console.WriteLine(sayHello);
+sayHello = sayHello.Replace("Hello", "Greetings");
+Console.WriteLine(sayHello);
+Console.WriteLine();
+
+// ToUpper and ToLower
+
+Console.WriteLine(sayHello.ToUpper());
+Console.WriteLine(sayHello.ToLower());
+Console.WriteLine();
