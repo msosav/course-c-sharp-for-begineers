78bd41b3ffb9974bcedbd0aa83cd8c8f37013b28
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-09-30 11:52:07 -0500

    Add Lesson 08: Branches (if)

---
+++ 08Branches/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Branches
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int a = 5;
+      int b = 6;
+      if (a + b > 10)
+      {
+        Console.WriteLine("The answer is greater than 10.");
+      }
+      else
+      {
+        Console.WriteLine("The answer is not greater than 10.");
+      }
+    }
+  }
+}
