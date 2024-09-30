c11e097090c9387d9270fab5768af2cb54809fc5
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-09-30 13:54:34 -0500

    Add Lesson 16: Objects and Classes

---
+++ Object Oriented Programming/MySuperBank/Classes/BankAccount.cs
@@ -0,0 +1,25 @@
+namespace Classes
+{
+  public class BankAccount
+  {
+    public string Number { get; }
+    public string Owner { get; set; }
+    public decimal Balance { get; }
+
+    public BankAccount(string owner, decimal balance)
+    {
+      this.Owner = owner;
+      this.Balance = balance;
+    }
+
+    public void MakeDeposit(decimal amount, DateTime date, string note)
+    {
+      
+    }
+
+    public void MakeWithdrawal(decimal amount, DateTime date, string note)
+    {
+      
+    }
+  }
+}
---
+++ Object Oriented Programming/MySuperBank/Program.cs
@@ -0,0 +1,13 @@
+﻿using Classes;
+
+namespace MySuperBank
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var account = new BankAccount("Soos", 10000);
+      Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+    }
+  }
+}
