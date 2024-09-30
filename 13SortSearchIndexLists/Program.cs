b65f95ec350faea693caba6623a4bc57769af1b9
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-09-30 12:39:23 -0500

    Add Lesson 13: Sort, Search, and Index Lists

---
+++ 13SortSearchIndexLists/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace SortSearchIndex
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var names = new List<string> { "<name>", "Ana", "Felipe" };
+      names.Add("Maria");
+      names.Add("Bill");
+      names.Remove("Ana");
+
+      var index = names.IndexOf("Felipe");
+      if (index != -1)
+      {
+        Console.WriteLine($"The name {names[index]} is at index {index}");
+      }
+
+      var notFound = names.IndexOf("Not Found");
+      Console.WriteLine($"When an item is not found, IndexOf returns {notFound}");
+
+      // Sort and then print the list
+      Console.WriteLine("\nBefore Sort");
+      foreach (var name in names)
+      {
+        Console.WriteLine($"Hello {name.ToUpper()}!");
+      }
+      Console.WriteLine("\nAfter Sort");
+      names.Sort();
+      foreach (var name in names)
+      {
+        Console.WriteLine($"Hello {name.ToUpper()}!");
+      }
+    }
+  }
+}
