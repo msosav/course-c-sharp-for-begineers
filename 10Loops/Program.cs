415f798d3d3138d65523fe11deeb1bcea5049233
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-09-30 12:09:05 -0500

    Add Lesson 10: What are Loops?

---
+++ 10Loops/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Loops
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("While loop");
+      int counter = 0;
+      while (counter < 10)
+      {
+        Console.WriteLine($"Hello World! The counter is {counter}");
+        counter++;
+      }
+      Console.WriteLine("\nFor loop");
+      for (int index = 0; index < 10; index++)
+      {
+        Console.WriteLine($"Hello World! The index is {index}");
+      }
+
+      Console.WriteLine("\nDo While loop");
+      int count = 0;
+      do
+      {
+        Console.WriteLine($"Hello World! The count is {count}");
+        count++;
+      } while (count < 10);
+    }
+  }
+}
