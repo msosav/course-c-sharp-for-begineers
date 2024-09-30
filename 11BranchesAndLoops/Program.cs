32f6f320f18c23506d37c7cbf1d04465dccf3e71
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-09-30 12:18:36 -0500

    Add Lesson 11: Combining Branches and Loops

---
+++ 11BranchesAndLoops/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace BranchesAndLoops {
+  class Program {
+    static void Main(string[] args) {
+      int sum = 0;
+      for (int i = 1; i <= 12; i++) {
+        if (i % 3 == 0) {
+          sum = sum + i;
+          Console.WriteLine($"The sum is {sum}");
+        }
+      }
+    }
+  }
+}
