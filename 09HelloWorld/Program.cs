0182d4465c8d2981406c3d09a6dbefc1e9817c2b
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-09-30 11:57:43 -0500

    Add Lesson 09: "Hello World" Explained

---
+++ 09HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System; // This imports the System namespace, which contains the Console class
+
+namespace HelloWorld // This is the namespace declaration, a namespace is a way to organize your code, and you are allowed to define your classes in a namespace
+{
+  class Program // This is a class declaration, the class Program contains the data and method definitions that your program uses
+  {
+    static void Main(string[] args) // This is a method declaration, the Main method is the entry point of a C# application
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
