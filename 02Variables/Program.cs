e41bafe87d994374de4ba22d5baa840a40e07a25
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-09-30 09:02:30 -0500

    Add Lesson 02: Hello World and Variables

---
+++ 02Variables/Program.cs
@@ -0,0 +1,10 @@
+﻿string aFriend = "Bill";
+Console.WriteLine(aFriend);
+
+// Change the value of aFriend to "Maira" and run the program again.
+
+aFriend = "Maira";
+Console.WriteLine(aFriend);
+
+// String Interpolation
+Console.WriteLine($"Hello, {aFriend}!");
