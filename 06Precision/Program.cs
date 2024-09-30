2cbbe2838fd64c54bb7a1def43ba341f8390e174
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-09-30 11:38:02 -0500

    Add Lesson 06: Numbers and Integer Precision

---
+++ 06Precision/Program.cs
@@ -0,0 +1,24 @@
+﻿int max = int.MaxValue;
+int min = int.MinValue;
+
+Console.WriteLine($"The range of integers is {min} to {max}");
+
+int what = max + 3;
+Console.WriteLine($"An example of overflow: {what}\n");
+
+// Working with doubles
+
+double a = 19;
+double b = 23;
+double c = 8;
+double d = (a + b) / c;
+Console.WriteLine(d);
+Console.WriteLine();
+
+double maxDouble = double.MaxValue;
+double minDouble = double.MinValue;
+
+Console.WriteLine($"The range of double is {minDouble} to {maxDouble}\n");
+
+double third = 1.0 / 3.0;
+Console.WriteLine(third);
