e256ca06cc7e812880347d503c0060bed6db606d
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-10-28 15:46:01 -0500

    Refactor BankAccount constructor to generate unique account numbers

--- Object Oriented Programming/MySuperBank/Classes/BankAccount.cs
+++ Object Oriented Programming/MySuperBank/Classes/BankAccount.cs
@@ -6,10 +6,14 @@ namespace Classes
     public string Owner { get; set; }
     public decimal Balance { get; }
 
+    private static int accountNumberSeed = 1234567890;
+
     public BankAccount(string owner, decimal balance)
     {
       this.Owner = owner;
       this.Balance = balance;
+      this.Number = accountNumberSeed.ToString();
+      accountNumberSeed++;
     }
 
     public void MakeDeposit(decimal amount, DateTime date, string note)
--- Object Oriented Programming/MySuperBank/Program.cs
+++ Object Oriented Programming/MySuperBank/Program.cs
@@ -8,6 +8,8 @@ namespace MySuperBank
     {
       var account = new BankAccount("Soos", 10000);
       Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+      var account2 = new BankAccount("Sooos", 10000);
+      Console.WriteLine($"Account {account2.Number} was created for {account2.Owner} with {account2.Balance} initial balance.");
     }
   }
 }
